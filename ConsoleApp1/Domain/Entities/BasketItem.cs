b007caedeb7ad3be96d6f8b6679bede60c638ac8
isikbuse83 <isikbuse83@gmail.com>
2025-05-30 10:10:22 +0300

    We added for products inside the basket

--- ConsoleApp1/Domain/Entities/BasketItem.cs
+++ ConsoleApp1/Domain/Entities/BasketItem.cs
@@ -1,23 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleApp1.Domain.Entities
 {
-    
+    [Owned]
     public class BasketItem
     {
-        [Key]
-        public int BasketItemId { get; set; }
+        public int ProductId { get; private set; }
+        public Product Product { get; }
     
-        public int BasketId { get; set; }
-        public Basket Basket { get; set; }
-    
-        public int ProductId { get; set; }
-        public Product Product { get; set; }
-    
-        public int Quantity { get; set; }
+        public int Quantity { get; private set; }
+
+        public BasketItem(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public void IncreaseQuantity()
+        {
+            Quantity++;
+        }
     }
-
-
-    
 }
