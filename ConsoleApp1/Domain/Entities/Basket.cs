b642dbcd23e0d7113dd6c0688fbcedf7e0a5a326
isikbuse83 <isikbuse83@gmail.com>
2025-05-30 10:13:36 +0300

    We added functons for BAsketItem

--- ConsoleApp1/Domain/Entities/Basket.cs
+++ ConsoleApp1/Domain/Entities/Basket.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using ConsoleApp1.Domain.Entities;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace ConsoleApp1.Domain.Entities
 {
     public class Basket
     {
-        [Key] public int BasketId { get; set; }
+        [Key] public int Id { get; private set; }
 
-        public int UserId { get; set; }
-        public User User { get; set; }
+        public int UserId { get; private set; }
+        public User User { get; }
 
-        public List<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+        private readonly List<BasketItem> _items = new();
+        public IReadOnlyCollection<BasketItem> Items => _items.AsReadOnly();
+
+        public Basket(int userId)
+        {
+            UserId = userId;
+        }
+        
+        public void AddBasketItem(BasketItem basketItem) => _items.Add(basketItem);
+        
+        public void ClearBasket() => _items.Clear();
+        
+        public void RemoveBasketItem(BasketItem item)
+        {
+            _items.Remove(item);
+        }
     }
-
 }
